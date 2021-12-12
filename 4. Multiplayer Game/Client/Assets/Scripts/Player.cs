9896365cf90698e1733b97277220d95e4662b2a7
MayKoder <sg.miquel@gmail.com>
2021-12-12 19:48:49 +0100

    Movement tilt

--- 4. Multiplayer Game/Client/Assets/Scripts/Player.cs
+++ 4. Multiplayer Game/Client/Assets/Scripts/Player.cs
@@ -12,6 +12,8 @@ public class Player : MonoBehaviour
 
     public float movementSpeed = 5.0f;
 
+    public Animator movementAnimator;
+
     void Start()
     {
         lives = 1;
@@ -25,6 +27,17 @@ public class Player : MonoBehaviour
 
         if (horizontal != 0.0f || vertical != 0.0f)
         {
+
+            //if(vertical != 0.0f && Mathf.Abs(transform.eulerAngles.x) < 38)
+            //{
+            //    transform.Rotate(transform.right, 100 * -vertical * Time.deltaTime);
+            //}
+            //else if(Mathf.Abs(transform.eulerAngles.x) < 38)
+            //{
+            //    transform.Rotate(transform.right, 100 * -vertical * Time.deltaTime);
+            //}
+            movementAnimator.SetFloat("blendTilt", vertical);
+
             transform.Translate(new Vector3(horizontal, vertical, 0.0f) * movementSpeed * Time.deltaTime, Space.World);
         }
     }
