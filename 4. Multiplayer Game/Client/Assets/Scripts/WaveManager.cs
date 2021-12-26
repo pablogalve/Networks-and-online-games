c07da482d01b4cacc9196edb2586a98d6ec5ae9a
MayKoder <sg.miquel@gmail.com>
2021-12-26 18:17:50 +0100

    Update WaveManager.cs

--- 4. Multiplayer Game/Client/Assets/Scripts/WaveManager.cs
+++ 4. Multiplayer Game/Client/Assets/Scripts/WaveManager.cs
@@ -56,11 +56,6 @@ public class WaveManager : MonoBehaviour
         if(waveCount == 0)
         {
             SpawnWave(waveCount);
-
-            if (waveCount >= 8)
-            {
-                WaveManager.instance.server.GameOver();
-            }
         }
     }
 
@@ -106,6 +101,11 @@ public class WaveManager : MonoBehaviour
         {
             waveCount++;
             current_enemies = 0;
+
+            if(waveCount >= 8)
+            {
+                WaveManager.instance.server.GameOver();
+            }
         }
     }
 }
