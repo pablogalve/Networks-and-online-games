81bd8d217c1b5beedfbb74bcf7169a20ef5c9116
pablogalve <pablogalve100@gmail.com>
2021-12-31 19:42:17 +0100

    boss death + projectile tag fix + shoot started

--- 4. Multiplayer Game/Client/Assets/Scripts/NetworkedObjects/Boss.cs
+++ 4. Multiplayer Game/Client/Assets/Scripts/NetworkedObjects/Boss.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,14 @@ public class Boss : MonoBehaviour
     public float timerBetweenAttacks = 5.0f;
     public float timerBetweenShots = 1.0f;
     public int totalShotsPerAttack = 5;
+
+    public GameObject destroyParticles;
+
+    [SerializeField]
+    private int points = 25;
+
+    public GameObject shootPoint;
+    public GameObject projectile;
 
     // Start is called before the first frame update
     void Start()
@@ -28,8 +37,11 @@ public class Boss : MonoBehaviour
 
     IEnumerator AttackCycle()
     {
+        // Wait between attacks
+        yield return new WaitForSeconds(timerBetweenAttacks);
+
         // Attack 1
-        for(int i = 0; i < totalShotsPerAttack; ++i)
+        for (int i = 0; i < totalShotsPerAttack; ++i)
         {
             Attack1();
             yield return new WaitForSeconds(timerBetweenShots);
@@ -53,10 +65,7 @@ public class Boss : MonoBehaviour
         {
             Attack3();
             yield return new WaitForSeconds(timerBetweenShots);
-        }
-
-        // Wait between attacks
-        yield return new WaitForSeconds(timerBetweenAttacks);
+        }        
 
         // Start the attack cycle again recursively
         StopAllCoroutines();
@@ -65,6 +74,9 @@ public class Boss : MonoBehaviour
 
     void Attack1()
     {
+        //PhotonNetwork.Instantiate(projectile.name, shootPoint.transform.position, Quaternion.Euler(0, 0, -15.0f));
+        PhotonNetwork.Instantiate(projectile.name, shootPoint.transform.position, Quaternion.identity);
+        //PhotonNetwork.Instantiate(projectile.name, shootPoint.transform.position, Quaternion.Euler(0, 0, 15.0f));
         Debug.Log("Attack 1");
         // Shoot
     }
@@ -83,6 +95,19 @@ public class Boss : MonoBehaviour
 
     void Die()
     {
-        Debug.Log("Enemy died");
+        GameManager.instance.AddScore(points);
+        WaveManager.IsWaveDone();
+
+        PhotonNetwork.Instantiate(destroyParticles.name, transform.position, Quaternion.identity);
+
+        PhotonNetwork.Destroy(gameObject);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("PlayerProjectile"))
+        {
+            lives--;
+        }
     }
 }
